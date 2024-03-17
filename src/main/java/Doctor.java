89ded3047abf0addbd1958d14c7c1f95b7df6fed
Qeadz <126616386+Qeadz@users.noreply.github.com>
2024-03-17 10:21:37 +0200

    Create Doctor.java

---
+++ src/main/java/Doctor.java
@@ -0,0 +1,37 @@
+public class Doctor {
+    private String name;
+    private String specialization;
+    private double rate;
+
+    // Constructor
+    public Doctor(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+        this.rate = 0;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+}
